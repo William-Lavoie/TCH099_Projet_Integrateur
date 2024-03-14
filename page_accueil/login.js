0de45d2b3126954cbda651abd2f92cb7f24f3b53
z-Myle <milena.busuioc.1@ens.etsmtl.ca>
2024-03-14 09:15:15 -0400

    Améliorations dans page_accueil

--- page_accueil/login.js
+++ page_accueil/login.js
@@ -6,7 +6,7 @@ auth0.createAuth0Client({
     }
   }).then(async (auth0Client) => {
     // Assumes a button with id "login" in the DOM
-    const loginButton = document.getElementById("login");
+    const loginButton = document.getElementById("connecter");
   
     loginButton.addEventListener("click", (e) => {
       e.preventDefault();
@@ -32,7 +32,7 @@ auth0.createAuth0Client({
     const userProfile = await auth0Client.getUser();
   
     // Assumes an element with id "profile" in the DOM
-    const profileElement = document.getElementById("profile");
+    const profileElement = document.getElementById("inscrire");
   
     if (isAuthenticated) {
       profileElement.style.display = "block";
