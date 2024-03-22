6981710288a4f26a3f573f357aa5a7222b3b82ed
William-Lavoie <william.lavoie.3@ens.etsmtl.ca>
2024-03-22 11:17:07 -0400

    filtering meetings by group

--- réunions/scripts/centre_de_réunions.js
+++ réunions/scripts/centre_de_réunions.js
@@ -52,7 +52,8 @@ document.addEventListener('DOMContentLoaded', function () {
     .then(data => {
      
         for (let i = 0; i < data.length; i++) {
-            $("#conteneur_reunions").append("<div class='reunion_header'><div id='titre_reunion'>" +  data[i]['titre'] + "</div> <button id='reglage_reunion'>⚙</button></div><div id='description_reunion'>" + data[i]['description'] + "</div></div>");
+            console.log(data[i]);
+            $("#conteneur_reunions").append("<div class='conteneur-reunion'><div class='reunion_header'><div id='titre_reunion'>" +  data[i]['titre'] + "</div> <button id='reglage_reunion'>⚙</button></div><div id='description_reunion'>" + data[i]['description'] + "</div></div></div>");
         }
        
        
