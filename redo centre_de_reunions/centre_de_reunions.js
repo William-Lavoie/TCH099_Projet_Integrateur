a2138baa001ff636ee6857d8344d8ce149983760
William-Lavoie <william.lavoie.3@ens.etsmtl.ca>
2024-03-24 12:24:15 -0400

    fixing centre de reunions

--- redo centre_de_reunions/centre_de_reunions.js
+++ redo centre_de_reunions/centre_de_reunions.js
@@ -1,3 +1,4 @@
+
 function basculerGroupes() {
     var groupes = document.getElementById("conteneur-groupes");
     var btnBasculer = document.getElementById("conteneur-basculer");
@@ -17,3 +18,32 @@ function basculerGroupes() {
         document.getElementById("basculer").innerText = "⪔";
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+
+     // Afficher les réunions 
+     fetch("http://127.0.0.1:3000/calendrier/api/api_calendrier.php/obtenir_reunions_utilisateur", {
+    })
+    .then(response => {
+
+    if (response.ok) {
+
+    return response.json();
+    }
+
+    else {
+    console.log("error");
+    }
+    })
+    .then(data => {
+    
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i]);
+            $("#conteneur-reunions").append("<div class='conteneur-reunion'><div class='reunion-entete'><p class='reunion-titre'>" +  data[i]['titre'] + "</p> <p class='reunion-reglage'>⚙</p></div><div class='reunion-description'>" + data[i]['description'] + "</div></div></div>");
+        }
+    
+    })
+    .catch(error => {
+
+    });
+});
