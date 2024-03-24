10cbab49118db43672330d901391e9c09a10536d
z-Myle <milena.busuioc.1@ens.etsmtl.ca>
2024-03-24 14:58:42 -0400

    Lisibilité code

--- redo centre_de_reunions/centre_de_reunions.js
+++ redo centre_de_reunions/centre_de_reunions.js
@@ -22,11 +22,116 @@ function basculerGroupes() {
     }
 }
 
-$(document).ready(function() {
+/*
+ * Affiche les réunions spécifiques à un groupe
+ */
 
+function afficherReunionsGroupe(groupe) {
+
+    $("#conteneur-reunions").html("");
+
+    let donnees = {'idGroupe': groupe};
+    // Afficher les réunions pour un groupe
+    fetch("http://127.0.0.1:3000/calendrier/api/api_calendrier.php/obtenir_reunions_groupes", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(donnees)
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            console.log("error");
+        }
+    })
+    .then(data => {
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i]);
+            $("#conteneur-reunions").append("<div class='conteneur-reunion'><div class='reunion-entete'><p class='reunion-titre'>" +  data[i]['titre'] + "</p> <p class='reunion-reglage'>⚙</p></div><div class='reunion-description'>" + data[i]['description'] + "</div></div></div>");
+
+            let nouvelleReunion = $("<div class='conteneur-reunion'><div class='reunion-entete'><p class='reunion-titre'>" +  data[i]['titre'] + "</p> <p class='reunion-reglage'>⚙</p></div><div class='reunion-description'>" + data[i]['description'] + "</div></div></div>");
+            $("#conteneur-reunions").append(nouvelleReunion);
+
+            // Ouvrir la réunion quand l'utilisateur clique dessus
+            nouvelleReunion.on("click", function() {
+                console.log("ok");
+                let idReunion = data[i]['id_reunions'];
+                window.location.href = "../../réunions/réunions.html?info=" + encodeURIComponent(idReunion);
+            });
+        }
+    })
+    .catch(error => {});
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    afficherReunion();
+
+    function afficherReunion() {
+        // Afficher les réunions 
+        fetch("http://127.0.0.1:3000/calendrier/api/api_calendrier.php/obtenir_reunions_utilisateur", {})
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                console.log("error");
+            }
+        })
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                let nouvelleReunion = $("<div class='conteneur-reunion'><div class='reunion-entete'><p class='reunion-titre'>" +  data[i]['titre'] + "</p> <p class='reunion-reglage'>⚙</p></div><div class='reunion-description'>" + data[i]['description'] + "</div></div></div>");
+                $("#conteneur-reunions").append(nouvelleReunion);
+
+                // Ouvrir la réunion quand l'utilisateur clique dessus
+                nouvelleReunion.on("click", function() {
+                    let idReunion = data[i]['id_reunions'];
+                    window.location.href = "../../réunions/réunions.html?info=" + encodeURIComponent(idReunion);
+                });
+            }
+        })
+        .catch(error => {});
+    }
+
+    // Afficher la liste des groupes
+    fetch("http://127.0.0.1:3000/calendrier/api/api_calendrier.php/afficher_groupes", {})
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            console.log("error");
+        }
+    })
+    .then(data => {
+        const groupesTable = $("#table-groupes");
+        for (let i = 0; i < data.length; i++) {
+            const nouveauGroupe = $("<button class='groupe'>").text(data[i]['nom']);
+            const nouvelleCellule = $("<td>").append(nouveauGroupe);
+            groupesTable.find("tr").last().after($("<tr>").append(nouvelleCellule));
+
+            nouveauGroupe.on("click", function(event) {
+                event.preventDefault();
+                // Si le groupe est déjà sélectionné, affiche toutes les réunions
+                if ($(this).hasClass("groupe-choisi")) {
+                    afficherReunion();
+                    $("button").removeClass("groupe-choisi");
+                } else {
+                    $("button").removeClass("groupe-choisi");
+                    $(this).addClass("groupe-choisi");
+                    afficherReunionsGroupe(data[i]['id_groupes']);
+                }
+            });
+        }
+    })
+    .catch(error => {
+        console.log("erreur");
+    });
+});
+
+$(document).ready(function() {
+    
     /*
-    * SÉLECTIONNER LES ÉLÉMENTS
-    */
+     * SÉLECTIONNER LES ÉLÉMENTS
+     */
 
     // Éléments pour créer un nouveau groupe
     const btnCreerGroupe = $("#table-creer-groupe");
@@ -40,33 +145,18 @@ $(document).ready(function() {
 
     let tableauParticipants = [];
 
-    /*
-     * CRÉER UN NOUVEAU GROUPE
-     */
-
-    /*
-     * Affiche les groupes dans le "aside"
-     */
-        function afficherGroupes() {
-            fetch("http://127.0.0.1:3000/calendrier/api/api_calendrier.php/afficher_groupes", {
-            })
-            .then(response => {
-        
+    // Afficher les groupes dans le "aside"
+    function afficherGroupes() {
+        fetch("http://127.0.0.1:3000/calendrier/api/api_calendrier.php/afficher_groupes", {})
+        .then(response => {
             if (response.ok) {
-        
-            return response.json();
+                return response.json();
+            } else {
+                console.log("error");
             }
-        
-            else {
-            console.log("error");
-            }
-            })
-            .then(data => {
-        
-              // Ajouter les groupes à la liste des groupes dans la sidebar
-    
-              for (let i = 0; i < data.length; i++) {
-    
+        })
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
                 const nouveauGroupe = $("<button class='groupe'>").text(data[i]['nom']);
                 const nouvelleCellule = $("<td>").append(nouveauGroupe);
                 groupesTable.find("tr").last().after($("<tr>").append(nouvelleCellule));
@@ -77,17 +167,60 @@ $(document).ready(function() {
                     $(this).addClass("groupe-choisi");
                     afficherReunionsGroupe(data[i]['id_groupes']);
                 })
-          
-              }
-                
+            }
+        })
+        .catch(error => {
+            console.log("erreur");
+        });
+    }
+
+    // Valider les champs avant de procéder avec le bouton "Confirmer"
+    btnConfirmer.click(function(event) {
+        event.preventDefault(); // Prévient le reload (Besoin API)
+
+        // Conserver les informations des champs
+        const nomGroupe = $("#nom-groupe").val().trim();
+        const description = $("#description").val().trim();
+        const participantsText = listeParticipants.text().trim(); // Obtenir le texte au complet
+        const nbParticipants = participantsText ? participantsText.split(", ").length : 0; // Nombre de participants
+
+        // Vérifier si le champ nom du groupe est valide
+        if (nomGroupe.length < 1) {
+            erreurs.text("Le nom du groupe doit contenir au moins un caractère.");
+        } else if (nbParticipants < 1) {
+            erreurs.text("Il doit y avoir au moins un participant.");
+        } else {
+            erreurs.text(""); // Vider les erreurs
+
+            // Si tous les champs sont valides, procéder à la soumission du formulaire
+            const donnees = {"nom": nomGroupe, "participants": tableauParticipants};
+
+            fetch("http://127.0.0.1:3000/calendrier/api/api_calendrier.php/ajouter_groupe", {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(donnees)
+            })
+            .then(response => {
+                if (response.ok) {
+                    window.location.reload();
+                } else {
+                    throw new Error("La création de la réunion n'a pas fonctionnée");
+                }
             })
             .catch(error => {
-        
-              console.log("erreur");
+                console.log(error);
             });
-        }
 
-        afficherGroupes();
+            // Cacher le formulaire (fin)
+            formulaireGroupe.css("visibility", "hidden");
+
+            // Réinitialiser les champs du formulaire
+            $("#nom-groupe").val('');
+            $("#description").val('');
+            listeParticipants.text('');
+            erreurs.text('');
+        }
+    });
 
     // Afficher le formulaire pour créer un groupe
     btnCreerGroupe.click(function() {
@@ -112,7 +245,7 @@ $(document).ready(function() {
         event.preventDefault();
 
         const participant = $("#ajouter-participants").val().trim();
-        console.log(tableauParticipants);
+        
         // Vérifier si le champ participant est valide
         if (participant === "") { // Si vide
             erreurs.text("Veuillez saisir une adresse e-mail.");
@@ -120,17 +253,15 @@ $(document).ready(function() {
         } else if (!validerAdresse(participant)) {
             erreurs.text("L'adresse e-mail est invalide.");
             return false;
-        } 
-        else if (tableauParticipants.includes(participant)) {
+        } else if (tableauParticipants.includes(participant)) {
             erreurs.text("Le participant est déjà dans la liste.");
             return false;
-        }  
-        else {
+        } else {
             // Vérifier s'il y a déjà des participants
             if (listeParticipants.text() === "") {
                 listeParticipants.text(participant);
 
-                    // Ajouter à la liste des participants
+                // Ajouter à la liste des participants
                 tableauParticipants.push(participant);
             } else {
                 // Ajouter le participant à la liste en séparant par une virgule et un espace
@@ -139,73 +270,10 @@ $(document).ready(function() {
                 // Ajouter à la liste des participants
                 tableauParticipants.push(participant);
             }
-            
+
             erreurs.text(""); // Vider les erreurs
             $("#ajouter-participants").val(""); // Effacer le champ d'ajout de participant
         }
-    });
-
-    // Valider les champs avant de procéder avec le bouton "Confirmer"
-    btnConfirmer.click(function(event) {
-        event.preventDefault(); // Prévient le reload (Besoin API)
-
-        // Conserver les informations des champs
-        const nomGroupe = $("#nom-groupe").val().trim();
-        const description = $("#description").val().trim();
-        const participantsText = listeParticipants.text().trim(); // Obtenir le texte au complèt
-        const nbParticipants = participantsText ? participantsText.split(", ").length : 0; // Nombre de participants
-
-        console.log(nomGroupe.length);
-        // Vérifier si le champ nom du groupe est valide
-        if (nomGroupe.length < 1) {
-            console.log("test");
-            erreurs.text("Le nom du groupe doit contenir au moins un caractère.");
-        } 
-
-        // Vérifier si le champ des participants est valide
-        else if (nbParticipants < 1) {
-            erreurs.text("Il doit y avoir au moins un participant.");
-        } 
-        
-        else {
-            erreurs.text(""); // Vider les erreurs
-        
-
-        // Si tous les champs sont valides, procéder à la soumission du formulaire
-        const donnees = {"nom": nomGroupe,
-                        "participants": tableauParticipants};
-
-        console.log(donnees);
-
-        fetch("http://127.0.0.1:3000/calendrier/api/api_calendrier.php/ajouter_groupe", {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(donnees)
-        })
-        .then(response => {
-
-            if (response.ok) {
-                window.location.reload();
-            }
-
-            else {
-                throw new Error("La création de la réunion n'a pas fonctionnée");
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
-
-        // Cacher le formulaire (fin)
-        formulaireGroupe.css("visibility", "hidden");
-
-        // Réinitialiser les champs du formulaire
-        $("#nom-groupe").val('');
-        $("#description").val('');
-        listeParticipants.text('');
-        erreurs.text('');
-
-    }
     });
 
     // Fonction pour valider le format d'une adresse e-mail
@@ -213,17 +281,4 @@ $(document).ready(function() {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return emailRegex.test(email);
     }
-
-    /*
-    * CRÉER UNE NOUVELLE RÉUNION
-    */
-
-    //function creerNouvelleReunion() {}
-
-    /*
-    * CHARGER LES RÉUNIONS PAR RAPPORT AU GROUPE SÉLECTIONNÉ
-    */
-
-    //function chargerReunions() {}
-
 });
