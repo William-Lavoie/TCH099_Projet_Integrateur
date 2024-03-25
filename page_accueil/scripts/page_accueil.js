5b114a84b7845f44b189077912e10c6b4713fbf0
EtiBarr <etienne.barriere.1@ens.etsmtl.ca>
2024-03-25 16:28:05 -0400

    work on landing page

--- page_accueil/scripts/page_accueil.js
+++ page_accueil/scripts/page_accueil.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let bureauAcceuil = document.getElementById("bureau-acceuil");
     
     button.addEventListener("click", function() {
+        //cacher elements apres en aprendre plus
         if(containerCafe.style.display === "none") {
             containerCafe.style.display = "";
         } else {
@@ -16,13 +17,38 @@ document.addEventListener("DOMContentLoaded", function() {
             bureauAcceuil.style.display = "none";
         }
 
-        let container = document.getElementById('conteneur-titre-page-accueil');
-        container.classList.toggle('collapse');
+        let containerTitrePage = document.getElementById('conteneur-titre-page-accueil');
+        containerTitrePage.classList.toggle('collapse');
 
         if (button.innerHTML === "En aprendre plus") {
-            button.innerHTML = "Fermer";
+            button.innerHTML = "▼";
         } else {
             button.innerHTML = "En aprendre plus";
         }
+
+        //afficher apres en aprendre plus
+        let containerAprendrePlus = document.getElementById('conteneur-aprendre-plus');
+        containerAprendrePlus.classList.toggle('collapse');
+
+        let h1Elements = document.querySelectorAll('#premiere-boite-aprendre h1');
+
+            h1Elements[0].classList.add('animate__animated' );
+            h1Elements[0].classList.add('animate__zoomIn');
+            
+            h1Elements[1].classList.add('cacher');
+
+        setTimeout(function() {
+
+
+            setTimeout(function() {
+                h1Elements[1].classList.remove('cacher');
+                h1Elements[1].classList.add('animate__animated');
+                h1Elements[1].classList.add('animate__zoomIn');
+            }, 600);
+        }, 100); 
     });
+
+
+
+
 });
