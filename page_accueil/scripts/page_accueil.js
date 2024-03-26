099883f099946f79128861abd79480cb03f75524
EtiBarr <etienne.barriere.1@ens.etsmtl.ca>
2024-03-25 20:54:39 -0400

    addition de slide page acceuil

--- page_accueil/scripts/page_accueil.js
+++ page_accueil/scripts/page_accueil.js
@@ -63,82 +63,163 @@ document.addEventListener("DOMContentLoaded", function() {
         let h1ElementsDeuxiemeBoite = document.querySelectorAll('#premier-message-boite-deux h1');
         let h1ElementsTroisiemeBoite = document.querySelectorAll('#troisieme-boite-aprendre h1');
         let h1ElementsQuatriemeBoite = document.querySelectorAll('#quatrieme-boite-aprendre h1');
+        let h1ElementsCinquiemeBoite = document.querySelectorAll('#cinquieme-boite-aprendre h1');
 
-            // action a 25% height pour la deuxime boite
-        if (scrollPercentage >= 25 && scrollPercentage < 50) {
+
+        // action a 20% height pour la deuxime boite
+        if (scrollPercentage >= 20 && scrollPercentage < 40) {
             h1ElementsDeuxiemeBoite[0].classList.add('animate__animated' );
             h1ElementsDeuxiemeBoite[0].classList.add('animate__slideInRight' );
 
 
-                //  action a 50% height pour la troisieme boite
-        } else if (scrollPercentage >= 50 && scrollPercentage < 75) {
+        //  action a 40% height pour la troisieme boite
+        } else if (scrollPercentage >= 40 && scrollPercentage < 60) {
 
             h1ElementsTroisiemeBoite[0].classList.add('animate__animated' );
             h1ElementsTroisiemeBoite[0].classList.add('animate__zoomIn' );
 
-          //  action a 75% height pour la quatrieme boite
-        } else if (scrollPercentage >= 90) {
+        //  action a 60% height pour la quatrieme boite
+        } else if (scrollPercentage >= 60 && scrollPercentage < 80) {
             h1ElementsQuatriemeBoite[0].classList.add('animate__animated' );
             h1ElementsQuatriemeBoite[0].classList.add('animate__zoomIn' );
+
+
+        //  action a 60% height pour la quatrieme boite
+        } else if (scrollPercentage >= 90) {
+            h1ElementsCinquiemeBoite[0].classList.add('animate__animated' );
+            h1ElementsCinquiemeBoite[0].classList.add('animate__zoomIn' );
         }
+        
     });
 
 
 
     //**************** */
-    //slider
+    //slide dans les conteneur
     //*************** */
-    const btnGauche = document.getElementById("btn-gauche-deuxieme-boite");
-    const btnDroite = document.getElementById("btn-droite-deuxieme-boite");
-    const premierMessage = document.getElementById("premier-message-boite-deux");
-    const deuxiemeMessage = document.getElementById("deuxieme-message-boite-deux");
-    const troisiemeMessage = document.getElementById("troisieme-message-boite-deux");
 
-    let currentSlide = 1;
 
-    btnGauche.addEventListener("click", function() {
-        if (currentSlide === 1) {
+    //deuxieme boite
+    const btnGaucheDeux = document.getElementById("btn-gauche-deuxieme-boite");
+    const btnDroiteDeux = document.getElementById("btn-droite-deuxieme-boite");
+    const premierMessageDeux = document.getElementById("premier-message-boite-deux");
+    const deuxiemeMessageDeux = document.getElementById("deuxieme-message-boite-deux");
+    const troisiemeMessageDeux = document.getElementById("troisieme-message-boite-deux");
+
+    let currentSlideDeux = 1;
+
+    btnGaucheDeux.addEventListener("click", function() {
+        if (currentSlideDeux === 1) {
             return; 
         }
-        currentSlide--;
-        updateSlideDisplay();
+        currentSlideDeux--;
+        updateSlideDisplayDeux();
     });
 
-    btnDroite.addEventListener("click", function() {
-        if (currentSlide === 3) {
+    btnDroiteDeux.addEventListener("click", function() {
+        if (currentSlideDeux === 3) {
             return;
         }
-        currentSlide++;
-        updateSlideDisplay();
+        currentSlideDeux++;
+        updateSlideDisplayDeux();
     });
 
-    function updateSlideDisplay() {
-        premierMessage.style.display = "none";
-        deuxiemeMessage.style.display = "none";
-        troisiemeMessage.style.display = "none";
+    function updateSlideDisplayDeux() {
+        premierMessageDeux.style.display = "none";
+        deuxiemeMessageDeux.style.display = "none";
+        troisiemeMessageDeux.style.display = "none";
     
-        if (currentSlide === 1) {
-            premierMessage.style.display = "flex";
-        } else if (currentSlide === 2) {
-            deuxiemeMessage.style.display = "flex";
-        } else if (currentSlide === 3) {
-            troisiemeMessage.style.display = "flex";
+        if (currentSlideDeux === 1) {
+            premierMessageDeux.style.display = "flex";
+        } else if (currentSlideDeux === 2) {
+            deuxiemeMessageDeux.style.display = "flex";
+        } else if (currentSlideDeux === 3) {
+            troisiemeMessageDeux.style.display = "flex";
         }
     
-        const indicators = document.querySelectorAll(".indicateur-de-slide");
-        indicators.forEach((indicator, index) => {
-            if (index === currentSlide - 1) {
-                indicator.style.backgroundColor = "#00c1f3"; 
-                indicator.style.transform = "scale(1.5)"; 
+        const indicatorsDeux = document.querySelectorAll(".indicateur-de-slide");
+        indicatorsDeux.forEach((indicatorsDeux, index) => {
+            if (index === currentSlideDeux - 1) {
+                indicatorsDeux.style.backgroundColor = "#00c1f3"; 
+                indicatorsDeux.style.transform = "scale(1.5)"; 
             } else {
-                indicator.style.backgroundColor = "white"; 
-                indicator.style.transform = "scale(1)"; 
+                indicatorsDeux.style.backgroundColor = "white"; 
+                indicatorsDeux.style.transform = "scale(1)"; 
             }
         });
     }
     
-    const firstIndicator = document.querySelector(".indicateur-de-slide");
-    firstIndicator.style.backgroundColor = "#00c1f3";
-    firstIndicator.style.transform = "scale(1.5)";
+    const premierIndicateurDeux = document.querySelector("#premier-indicateur-deux");
+    premierIndicateurDeux.style.backgroundColor = "#00c1f3";
+    premierIndicateurDeux.style.transform = "scale(1.5)";
+
+
+
+
+    //troisieme boite
+    const btnGaucheTrois = document.getElementById("btn-gauche-troisieme-boite");
+    const btnDroiteTrois = document.getElementById("btn-droite-troisieme-boite");
+    const premierMessageTrois = document.getElementById("premier-message-boite-trois");
+    const deuxiemeMessageTrois = document.getElementById("deuxieme-message-boite-trois");
+    const troisiemeMessageTrois = document.getElementById("troisieme-message-boite-trois");
+
+    let currentSlideTrois = 1;
+
+    btnGaucheTrois.addEventListener("click", function() {
+        if (currentSlideTrois === 1) {
+            return; 
+        }
+        currentSlideTrois--;
+        updateSlideDisplayTrois();
+    });
+
+    btnDroiteTrois.addEventListener("click", function() {
+        if (currentSlideTrois === 3) {
+            return;
+        }
+        currentSlideTrois++;
+        updateSlideDisplayTrois();
+    });
+
+    function updateSlideDisplayTrois() {
+        premierMessageTrois.style.display = "none";
+        deuxiemeMessageTrois.style.display = "none";
+        troisiemeMessageTrois.style.display = "none";
+    
+        if (currentSlideTrois === 1) {
+            premierMessageTrois.style.display = "flex";
+        } else if (currentSlideTrois === 2) {
+            deuxiemeMessageTrois.style.display = "flex";
+        } else if (currentSlideTrois === 3) {
+            troisiemeMessageTrois.style.display = "flex";
+        }
+    
+        const indicatorsTrois = document.querySelectorAll(".indicateur-de-slide-dark");
+        indicatorsTrois.forEach((indicatorsTrois, index) => {
+            if (index === currentSlideTrois - 1) {
+                indicatorsTrois.style.backgroundColor = "#00c1f3"; 
+                indicatorsTrois.style.transform = "scale(1.5)"; 
+            } else {
+                indicatorsTrois.style.backgroundColor = "black"; 
+                indicatorsTrois.style.transform = "scale(1)"; 
+            }
+        });
+    }
+    
+    const premierIndicateurTrois = document.querySelector("#premier-indicateur-trois");
+    premierIndicateurTrois.style.backgroundColor = "#00c1f3";
+    premierIndicateurTrois.style.transform = "scale(1.5)";
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
