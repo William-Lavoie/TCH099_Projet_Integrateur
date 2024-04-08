d3647622b3ab9ef51b05325de7914e21e9244fa7
EtiBarr <etienne.barriere.1@ens.etsmtl.ca>
2024-04-08 12:38:53 -0400

    jwt not done yet

---
+++ page_accueil/api/JWTValidation.php
@@ -0,0 +1,86 @@
+<?php
+
+header("Access-Control-Allow-Origin: http://127.0.0.1:3000, https://huddleharbor.com");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+//auth0
+use Auth0\SDK\Auth0;
+use Auth0\SDK\Configuration\SdkConfiguration;
+//library de validation (composer require lcobucci/jwt (library utiliser pour valider))
+use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Validation\Constraint\SignedWith;
+use Lcobucci\JWT\Validation\Validator;
+
+require __DIR__ . '/vendor/autoload.php'; // Include Composer autoloader
+
+//configer au auth0 SDK
+$config = new SdkConfiguration(
+    strategy: SdkConfiguration::STRATEGY_API,
+    domain: 'https://projet-integrateur-eq2.us.auth0.com',
+    audience: ['https://HHValidation/api']
+);
+
+$auth0 = new Auth0($config);
+
+//recoi token du post
+$token = $_POST['token'] ?? null;
+
+if (!$token) {
+    echo json_encode(['error' => 'Pas de Token']);
+    exit;
+}
+
+ // Decode le token
+try {
+    $decodedToken = $auth0->decode($token);
+} catch (\Auth0\Exception\CoreException $e) { //exception thrown par auth0
+    echo json_encode(['error' => 'Impossible de décoder le token']);
+    return;
+}
+
+// Get le JWKS de auth0
+$jwksUrl = 'https://projet-integrateur-eq2.us.auth0.com/.well-known/jwks.json';
+$jwksJson = file_get_contents($jwksUrl);
+$jwks = json_decode($jwksJson, true);
+
+
+$signing_key = null;
+
+//trouver cle dans le set
+foreach ($jwks['keys'] as $key) {
+    if ($decodedToken->header('kid') === $key['kid']) {
+        $signing_key = $key;
+        break;
+    }
+}
+
+if (!$signing_key) {
+    // Key not found
+    echo json_encode(['error' => 'Clé introuvable']);
+} else{
+
+    // Configuration for JWT
+    $jwtConfig = Configuration::forSymmetricSigner(
+        new \Lcobucci\JWT\Signer\Hmac\Sha256(), 
+        JWK::createFromKeyFile($signing_key)
+    );
+    
+    $validator = new Validator();
+    $constraints = [
+        new SignedWith(new RS256(), $jwtConfig)
+    ];
+
+// Verification de token
+$jwsVerifier = new JWSVerifier(new RS256());
+    if ($validator->validate($decodedToken, ...$constraints)) {
+    // Token est valide
+    echo json_encode(['success' => 'Token est valid']);
+} else {
+    // Token n'est pas valide
+    echo json_encode(['error' => 'Token n\'est pas valid']);
+}
+
+}
+?>
+
--- page_accueil/scripts/login.js
+++ page_accueil/scripts/login.js
@@ -1,92 +1,133 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-    //declaration des path dynamic
+document.addEventListener("DOMContentLoaded", function () {
+  //declaration des path dynamic
     const pathDynamic = window.location.origin;
 
-auth0.createAuth0Client({
+    auth0
+        .createAuth0Client({
         domain: "projet-integrateur-eq2.us.auth0.com",
         clientId: "nrLsb1vilAv0TV5kTpyqmP7Gt0NfiXcs",
         authorizationParams: {
-                redirect_uri: pathDynamic + "/calendrier/calendrier.html" 
-        }
-    }).then(async (auth0Client) => {
-
+            redirect_uri: pathDynamic + "/calendrier/calendrier.html",
+        },
+        })
+        .then(async (auth0Client) => {
         const loginButton = document.getElementById("connecter");
         const inscriptionButton = document.getElementById("inscrire");
-        
-        if(loginButton && inscriptionButton){
- //bouton d'authentification
-        loginButton.addEventListener("click", (e) => {
+
+        //envoi l'utilisateur a la page d'authentification apres l'appuie du bouton de 
+        if (loginButton && inscriptionButton) {
+            //bouton d'authentification
+            loginButton.addEventListener("click", (e) => {
             e.preventDefault();
             auth0Client.loginWithRedirect();
-        });
+            });
 
-        //bouton d'inscription
-        inscriptionButton.addEventListener("click", (e) => {
+            //bouton d'inscription
+            inscriptionButton.addEventListener("click", (e) => {
             e.preventDefault();
-            auth0Client.loginWithRedirect({authorizationParams: {
+            auth0Client.loginWithRedirect({
+                authorizationParams: {
                 screen_hint: "signup",
-            }})
-        });
-        }else{
-
-
-            if (typeof location.search === 'string' &&
-            (location.search.includes("state=") && 
-            (location.search.includes("code=") || 
-            location.search.includes("error=")))) {
+                },
+            });
+            });
+        } else {
+            if (
+            typeof location.search === "string" &&
+            location.search.includes("state=") &&
+            (location.search.includes("code=") ||
+                location.search.includes("error="))
+            ) {
             await auth0Client.handleRedirectCallback();
             window.history.replaceState({}, document.title, "/");
-        }
-    
+            }
 
-        // Assumes a button with id "logout" in the DOM
-        const logoutButton = document.getElementById("deconnexion");
-    
-        logoutButton.addEventListener("click", (e) => {
+            const logoutButton = document.getElementById("deconnexion");
+
+            logoutButton.addEventListener("click", (e) => {
             e.preventDefault();
             auth0Client.logout();
-        });
+            });
 
-    const isAuthenticated = await auth0Client.isAuthenticated();
+            const isAuthenticated = await auth0Client.isAuthenticated();
 
+            //verifie si l'utilisateur n'est pas authentifier, renvoi a la page d'acceuil
+            if (!isAuthenticated) {
+            if (
+                window.location.hostname === "localhost:" ||
+                window.location.hostname === "127.0.0.1:"
+            ) {
+                window.location.href =
+                window.location.protocol +
+                window.location.hostname +
+                window.location.port +
+                "index.html";
+            } else {
+                window.location.href = "https://huddleharbor.com";
+            }
+            return;
+            }
 
-    //******************absolutly awful and annoying during testing, so taking this for now ***************** */
-    //verifie si l'utilisateur est authentifie, si il ne l'ai pas, redirect to landing page
-    //if (!isAuthenticated) {
-        // Redirect to the login page if the user is not authenticated
-        //window.location.href = 'https://huddleharbor.com';
- //     return;
- // }
+            //recevoir token apres l'authentification
+            const token = await auth0Client.getTokenSilently();
 
-    //Si l'utilisateur est authentifier, utuliser les info
-    const userProfile = await auth0Client.getUser();
+            const userProfile = await auth0Client.getUser();
 
-    //const userId = userProfile.user_id; //****************************************** */
-
-            // profile de Auth0
-            const profileElement = document.getElementById("profile");
-
-            // Envoie de l'identifiant de l'utilisateur 
-            const identifiants = {"courriel": userProfile.name};
-
-            fetch(pathDynamic + "/calendrier/api/api_calendrier.php/envoyer_identifiant", {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(identifiants)
+            // Evoi le token au backend
+            fetch(pathDynamic + "/page_accueil/api/JWTValidation.php/valider_token", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({ token: token }),
             })
-            .then(response => {
-    
-            if (response.ok) {
-            }
-            else {
+            .then((response) => {
+                if (response.ok) {
+                //Si l'utilisateur est authentifier et le token valider, utuliser les info
+
+                // Envoie de l'identifiant de l'utilisateur
+                const identifiants = { courriel: userProfile.name };
+
+                fetch(
+                    pathDynamic +
+                    "/calendrier/api/api_calendrier.php/envoyer_identifiant",
+                    {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(identifiants),
+                    }
+                ).then((response) => {
+                    if (response.ok) {
+                    } else {
                     console.log("La requête n'a pas fonctionnée");
-            }
+                    }
+                });
+                } else {
+                console.log("error");
+                // si le token n'est pas valid, renvoi l'utilisateur a la page d'acceuil
+                if (
+                    window.location.hostname === "localhost:" ||
+                    window.location.hostname === "127.0.0.1:"
+                ) {
+                    window.location.href =
+                    window.location.protocol +
+                    window.location.hostname +
+                    window.location.port +
+                    "index.html";
+                } else {
+                    window.location.href = "https://huddleharbor.com";
+                }
+                }
+            })
+            .catch((error) => {
+                // error avec la validation de token
+                console.log(error);
             });
         }
-
-    }).catch(error => {
+        })
+        .catch((error) => {
+        //erreur avec auth
         console.log(error);
-    });
-
+        });
 });
