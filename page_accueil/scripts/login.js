cb57fc83f241424f3de7466fdf60a620dade747b
EtiBarr <etienne.barriere.1@ens.etsmtl.ca>
2024-04-21 00:41:42 -0400

    not a smooth experience, but an experience nonetheless
    it took me an embarrassing amount of time to think of this solution, but this is so that the user can go straight to the website if the session is still valid. Hoping to find a smoother resolution

--- page_accueil/scripts/login.js
+++ page_accueil/scripts/login.js
@@ -12,11 +12,19 @@ document.addEventListener("DOMContentLoaded", function () {
         },
         })
         .then(async (auth0Client) => {
+            
+        const isAuthenticated = await auth0Client.isAuthenticated();
+
         const loginButton = document.getElementById("connecter");
         const inscriptionButton = document.getElementById("inscrire");
 
         //envoi l'utilisateur a la page d'authentification apres l'appuie du bouton de 
         if (loginButton && inscriptionButton) {
+            //verifier si l'utilisateur est deja authentifier a la page d'acceuil
+            if(isAuthenticated){
+                window.location.href = pathDynamic + "/calendrier/calendrier.html";
+            }
+
             //bouton d'authentification
             loginButton.addEventListener("click", (e) => {
             e.preventDefault();
@@ -50,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
             auth0Client.logout();
             });
 
-            const isAuthenticated = await auth0Client.isAuthenticated();
+
 
             //verifie si l'utilisateur n'est pas authentifier, renvoi a la page d'acceuil
             if (!isAuthenticated) {
@@ -68,6 +76,9 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             return;
             }
+            
+            
+            
 
             const userProfile = await auth0Client.getUser();
 
@@ -109,6 +120,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     
                     // si le token n'est pas valid, renvoi l'utilisateur a la page d'acceuil
                     window.location.href = "https://huddleharbor.com";
+
                     }
                 });
             
